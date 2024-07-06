ab87d230b223d5b0c0fc134572b01a428647f412
Tomistoma <73132452+Tomistoma@users.noreply.github.com>
2024-07-06 21:02:30 +0200

    first

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+var cors = require('cors')
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+app.use(cors())
+app.use(express.static('public'));
+
+// Middleware to serve static files from the "public" directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Serve the index.html file for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://127.0.0.1:${PORT}/`);
+});
